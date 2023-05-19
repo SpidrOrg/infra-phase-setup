93c6fa39e95b384c9dcad7cc91d320ac128e19c8
visdxebia <vishal.daga@xebia.com>
2023-05-19 08:58:00 +0530

    init

--- bin/infra-phase-setup.js
+++ bin/infra-phase-setup.js
@@ -3,19 +3,21 @@
 const cdk = require('aws-cdk-lib');
 const { InfraPhaseSetupStack } = require('../lib/infra-phase-setup-stack');
 
+const indexOfAwsAccountInArnSplit = process.env.CODEBUILD_BUILD_ARN.split(":").indexOf(process.env.AWS_REGION) + 1;
+const awsAccount = process.env.CODEBUILD_BUILD_ARN.split(":")[indexOfAwsAccountInArnSplit];
+const awsRegion = process.env.AWS_REGION;
+const codestarConnArn = process.env.MP_GITHUB_CONN_ARN;
+const ingestionAwsRepo = process.env.MP_INGESTION_AWS_REPO.split("/");
+const transformationAwsRepo = process.env.MP_TRANSFORMATION_AWS_REPO.split("/");
+const platformTfRepo = process.env.MP_PLATFORM_TF_REPO.split("/");
+const envName = process.env.MP_ENV_NAME;
+
 const app = new cdk.App();
 new InfraPhaseSetupStack(app, 'InfraPhaseSetupStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { account: awsAccount, region: awsRegion },
+  envName,
+  codestarConnArn,
+  ingestionAwsRepo,
+  transformationAwsRepo,
+  platformTfRepo
 });
